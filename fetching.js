989fb785ea90e548db0edbb42fc2acd977ba812d
TurtledIn <inturtled@gmail.com>
2025-11-07 10:38:47 +0400

    javaScriptTesting

---
+++ fetching.js
@@ -0,0 +1,22 @@
+
+
+fetch("https://accounts.rec.net/account/me/profileimage", {
+    "headers": {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "authorization": "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ill3ckZ1MFFYTmp5SWhxZXBIN0dlamZwOE9nTSIsInR5cCI6ImF0K2p3dCIsIng1dCI6Ill3ckZ1MFFYTmp5SWhxZXBIN0dlamZwOE9nTSJ9.eyJuYmYiOjE3NjIyNDQ0NjQsImV4cCI6MTc2MjI0ODA2NCwiaXNzIjoiaHR0cHM6Ly9hdXRoLnJlYy5uZXQiLCJjbGllbnRfaWQiOiJyZWNuZXQiLCJyb2xlIjoid2ViQ2xpZW50Iiwic3ViIjoiODI1MjY1NDEiLCJhdXRoX3RpbWUiOjE3NjIyNDQ0NjMsImlkcCI6ImxvY2FsIiwianRpIjoiMjg3NEY5MjcwRDAwNzJFNjY3QzFFRDQzRjM5Q0ZEMkMiLCJzaWQiOiJBM0M5QkI4QkRBMEIyOTQzODVGMjc5MkE2MzcyNEVERCIsImlhdCI6MTc2MjI0NDQ2NCwic2NvcGUiOlsib3BlbmlkIiwicm4iLCJvZmZsaW5lX2FjY2VzcyJdLCJhbXIiOlsicHdkIl19.uUBcE2uF9mzjnobwMFLPLDWEYzeGR0Nb2rAQpzAX2r1OXG70gJkl6SC7qTeCbz5C8P3Y6YJnKUdKT_JId4z7m6TE6y9YfZ9ccO9mEMd9RPBfy5rNwMYjjIWDTviFAam3Pq1qOuuCyykp5g-V9NPWvtcH2-S8IOFEHnP_hU2iXRKpnkURAECMHA6QzMZBwfg-3IOjJRdV1yN7Pf9MWO0UM6swUc8pMS0X3jXZKFuEWO1UHt61-7hQXX81MJBA9nTUIFymqhHpsSjBKHdnmqovaxrRC5mxkbmXup2SJpdp16g63oLhPPV5MbJzi8_nDMQcT06VvwsPTt4DhWFtX-wcGA",
+        "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryFuYJk23oBhqGsJ8Y",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Google Chrome\";v=\"141\", \"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"141\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site"
+    },
+    "referrer": "https://rec.net/",
+    "body": "------WebKitFormBoundaryFuYJk23oBhqGsJ8Y\r\nContent-Disposition: form-data; name=\"imageName\"\r\n\r\n7bswahxqcctepdo1y6cr52x07.jpg\r\n------WebKitFormBoundaryFuYJk23oBhqGsJ8Y--\r\n",
+    "method": "PUT",
+    "mode": "cors",
+    "credentials": "include"
+});
